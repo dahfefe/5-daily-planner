b81a8812a7725f4752773994c647d64126ff03fb
Felix <157293782+dahfefe@users.noreply.github.com>
2024-03-17 01:56:09 -0700

    inserted empty function for everytime page prints to clear the text input areas

--- script.js
+++ script.js
@@ -47,13 +47,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -75,9 +69,19 @@ function saveDataToStorage(data) {
 
 function printEventData() {
 
-  var timeBlocks = [ 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  textAreaNineInput.empty();
+  textAreaTenInput.empty();
+  textAreaElevenInput.empty();
+  textAreaTwelveInput.empty();
+  textAreaThirteenInput.empty(); 
+  textAreaFourteenInput.empty();
+  textAreaFifthteenInput.empty();
+  textAreaSixteenInput.empty();
+  textAreaSeventeenInput.empty();
 
+  var timeBlocks = [ 9, 10, 11, 12, 13, 14, 15, 16, 17];
   var currentHour = dayjs().format('HH');
+  console.log(currentHour);
 
   for (var i = 0; i < timeBlocks.length; i++) {  
     let hourDiv = document.getElementById(`hour-${timeBlocks[i]}`)
